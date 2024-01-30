44fd403388dad51e0d25b8eff6f819ba2db144db
JarbasAi <jarbasai@mailfence.com>
2024-01-30 19:48:28 +0000

    :tada:

--- ovos_ocp_qml_gui/utils.py
+++
@@ -1,7 +0,0 @@
-from os.path import exists
-
-
-def is_qtav_available():
-    return exists("/usr/include/qt/QtAV") or \
-           exists("/usr/lib/qt/qml/QtAV") or \
-           exists("/usr/lib/libQtAV.so")
